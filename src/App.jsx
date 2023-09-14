a963c1d16d2d26811046c07d2c6ef49c6feabdf8
nganga mbugua <jobugua97@gmail.com>
2023-09-14 11:11:10 +0000

    new commits

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Job Mbugua Nganga",
+  title: "Web Developer & Cloud deveveloper",
+  email: "jobugua97@gmail.com",
   gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  instagram: "mbugua97",
+  linkedIn: "mbugua97",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mbugua97",
+  youTube: "mbugua97",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -23,18 +23,16 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+const description = "I'm a computer science student  studying at Laikipia university University.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web Developer",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
+  "cloud developer",
   "Mobile user interfaces",
   "Graphic design",
 ];
